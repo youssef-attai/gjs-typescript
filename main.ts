187a28021321f9bc298c264b882ae72f1192928f
youssef-attai <youssefgalalnazem@gmail.com>
2023-03-26 06:19:30 +0200

    create and show a GTK window

--- main.ts
+++ main.ts
@@ -3,4 +3,12 @@ import * as Gtk from "./@types/Gjs/Gtk-3.0";
 
 Gtk.init(null);
 
+const window = new Gtk.Window({
+  name: "Gjs + TypeScript",
+  default_width: 200,
+  default_height: 200,
+});
+
+window.show_all();
+
 Gtk.main();
